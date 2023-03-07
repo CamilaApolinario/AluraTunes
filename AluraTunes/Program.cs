e2d3c9802dc8e2e3caf34dbc73880b20bcaa736e
Camila Apolinario <Ka_millapolinario@hotmail.com>
2023-03-07 15:21:10 -0300

    Como ordenar, somar, agrupar uma consulta.

--- AluraTunes/Program.cs
+++ AluraTunes/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Xml.Linq;
 
 namespace AluraTunes
@@ -56,6 +57,7 @@ namespace AluraTunes
                 Console.WriteLine("{0}\t{1}\t{2}", musica.m.Id, musica.m.Nome, musica.g.Nome);
             }
 
+            Console.WriteLine("Digite alguma tecla para continuar...");
             Console.ReadLine();
 
 
@@ -97,7 +99,7 @@ namespace AluraTunes
             using (var contexto = new AluraTunesEntities())
             {
                 var query3 = from g in contexto.Generos
-                            select g;
+                             select g;
                 foreach (var genero in query)
                 {
                     Console.WriteLine("{0}\t{1}", genero.Id, genero.Nome);
@@ -167,21 +169,240 @@ namespace AluraTunes
                     Console.WriteLine("{0}\t{1}", artista.ArtistaId, artista.Nome);
                 }
 
+                Console.WriteLine("Digite alguma tecla para continuar...");
+                Console.ReadLine();
+            }
 
+            //Refinando consultas por parametros
+            Console.WriteLine("Refinando consultas por parametros");
+            using (var contexto = new AluraTunesEntities())
+            {
+                GetFaixas(contexto, "Led Zeppelin", "");
+                Console.WriteLine();
+                GetFaixas(contexto, "Led Zeppelin", "Graffiti");
+
+                Console.WriteLine("Digite alguma tecla para continuar...");
+                Console.ReadLine();
+            }
+
+            //Consultas ordenadas
+            Console.WriteLine("Consultas ordenadas");
+            using (var contexto = new AluraTunesEntities())
+            {
+                GetFaixasOrdenacao(contexto, "Led Zeppelin", "");
+                Console.WriteLine();
+                GetFaixasOrdenacao(contexto, "Led Zeppelin", "Graffiti");
+
+                Console.WriteLine("Digite alguma tecla para continuar...");
+                Console.ReadLine();
+            }
+
+            //Realizando contagem a partir de de uma consulta linq
+            Console.WriteLine("Contagem de uma consulta");
+            using (var contexto = new AluraTunesEntities())
+            {
+                //sintaxe de consulta
+                var query3 = from f in contexto.Faixa
+                             where f.Album.Artista.Nome == "Led Zeppelin"
+                             select f;
+
+                var quantidade = query.Count();
+                Console.WriteLine("Led Zeppelin tem {0} faixas de música.", quantidade);
+
+                //sintaxe de metodo
+                var quantidade1 = contexto.Faixa.Count(f => f.Album.Artista.Nome == "Led Zeppelin");
+
+                Console.WriteLine("Led Zeppelin tem {0} faixas de música.", quantidade1);
+                Console.WriteLine("Digite alguma tecla para continuar...");
+                Console.ReadLine();
+
+            }
+
+            //Soma de uma consulta linq
+            Console.WriteLine("Soma de uma consulta linq");
+            using (var contexto = new AluraTunesEntities())
+            {
+                var query4 = from inf in contexto.ItemNotaFiscal
+                             where inf.Faixa.Album.Artista.Nome == "Led Zeppelin"
+                             select new 
+                             { 
+                                 totalDoItem = inf.Quantidade * inf.PrecoUnitario 
+                             };
+
+                var totalDoArtista = query4.Sum(x => x.totalDoItem);
+                Console.WriteLine("Total do artista:R$ {0}.", totalDoArtista);
+
+                Console.WriteLine("Digite alguma tecla para continuar...");
+                Console.ReadLine();
+
+            }
+
+            //Agrupando item de uma consulta
+            Console.WriteLine("Agrupando item de uma consulta");
+            using (var contexto = new AluraTunesEntities())
+            {
+                var query5 = from inf in contexto.ItemNotaFiscal
+                             where inf.Faixa.Album.Artista.Nome == "Led Zeppelin"
+                             group inf by inf.Faixa.Album into agrupado
+                             // let é a declaração de uma váriavél dentro de uma consulta, assim não fica codigo repetido
+                             let vendasPorAlbum = agrupado.Sum(a => a.Quantidade * a.PrecoUnitario)
+                             orderby vendasPorAlbum descending
+                             //orderby agrupado.Sum(a => a.Quantidade * a.PrecoUnitario) descending
+                             select new
+                             {
+                                 TituloDoAlbum = agrupado.Key.Titulo,
+                                 TotalPorAlbum = vendasPorAlbum,
+                                 //TotalPorAlbum = agrupado.Sum(a=>a.Quantidade * a.PrecoUnitario)
+                             };
+
+                foreach (var agrupado in query5)
+                {
+                    Console.WriteLine("{0}\t{1}",
+                        agrupado.TituloDoAlbum.PadRight(40),
+                        agrupado.TotalPorAlbum);
+                }
+
+                Console.WriteLine("Digite alguma tecla para continuar...");
+                Console.ReadLine();
+            }
+
+            //Agrupando item de uma consulta
+            Console.WriteLine("Agrupando item de uma consulta");
+            using (var contexto = new AluraTunesEntities())
+            {
+                contexto.Database.Log = Console.WriteLine;
+
+                //dessa maneira criaria 3 consultas diferentes no banco de dados
+                var maiorVenda = contexto.NotaFiscal.Max(nf => nf.Total);
+                var menorVenda = contexto.NotaFiscal.Min(nf => nf.Total);
+                var vendaMedia = contexto.NotaFiscal.Average(nf => nf.Total);
+
+                Console.WriteLine("A maior venda é de {0}", maiorVenda);
+                Console.WriteLine("A maior venda é de {0}", menorVenda);
+                Console.WriteLine("A maior venda é de {0}", vendaMedia);
+                Console.WriteLine();
+
+                //É possivel fazer uma consulta só economizaremos no processamento do servidor SQL, no tráfico de rede e no tempo de processamento.
+
+                var vendas = (from nf in contexto.NotaFiscal
+                              group nf by 1 into agrupado
+                              select new
+                              {
+                                  MaiorVenda = agrupado.Max(nf => nf.Total),
+                                  MenorVenda = agrupado.Min(nf => nf.Total),
+                                  VendaMedia = agrupado.Average(nf => nf.Total)
+                              }).Single(); //Esse metodo transforma a query em um objeto, com três propriedades
+
+                Console.WriteLine("A maior venda é de R$ {0}", vendas.MaiorVenda);
+                Console.WriteLine("A menor venda é de R$ {0}", vendas.MenorVenda);
+                Console.WriteLine("A venda média é de R$ {0}", vendas.VendaMedia);
+
+                Console.WriteLine("Digite alguma tecla para continuar...");
+                Console.ReadLine();
+            }
+
+            // Usando linq para calcular a mediana de uma consulta
+            using (var contexto = new AluraTunesEntities())
+            {
+                var vendaMedia = contexto.NotaFiscal.Average(nf => nf.Total);
+                Console.WriteLine("Venda Média: {0}", vendaMedia);
+
+                var query5 = from nf in contexto.NotaFiscal
+                             select nf.Total;
+
+                //chamando o método comum
+                decimal mediana = Mediana(query5);
+                Console.WriteLine("Mediana: {0}", mediana);
+
+                //chamando o método de extensão
+                var vendaMediana = contexto.NotaFiscal.Mediana(nf => nf.Total);
+                Console.WriteLine("Mediana (com método de extensão): {0}", vendaMediana);
+
+                Console.WriteLine("Digite alguma tecla para continuar...");
                 Console.ReadLine();
             }
         }
-    }
-}
 
-class Genero
-{
-    public int Id { get; set; }
-    public string Nome { get; set; }
-}
-class Musica
-{
-    public int Id { get; set; }
-    public string Nome { get; set; }
-    public int GeneroId { get; set; }
+        private static void GetFaixas(AluraTunesEntities contexto, string buscaArtista, string buscaAlbum)
+        {
+            var query = from f in contexto.Faixa
+                        where f.Album.Artista.Nome.Contains(buscaArtista)
+                        select f;
+            if (!string.IsNullOrEmpty(buscaAlbum))
+            {
+                query = query.Where(q => q.Album.Titulo.Contains(buscaAlbum));
+            }
+            foreach (var faixa in query)
+            {
+                Console.WriteLine("{0}\t{1}", faixa.Album.Titulo.PadRight(40), faixa.Nome);
+            }
+        }
+
+        //Metodo comum
+        private static decimal Mediana(IQueryable<decimal> query)
+        {
+            var contagem = query.Count();
+
+            var queryOrdenada = query.OrderBy(total => total);
+
+            var elementCentral_1 = queryOrdenada.Skip(contagem / 2).First();
+
+            var elementCentral_2 = queryOrdenada.Skip((contagem - 1) / 2).First();
+
+            var mediana = (elementCentral_1 + elementCentral_2) / 2;
+
+            return mediana;
+        }
+
+        private static void GetFaixasOrdenacao(AluraTunesEntities contexto, string buscaArtista, string buscaAlbum)
+        {
+            var query = from f in contexto.Faixa
+                        where f.Album.Artista.Nome.Contains(buscaArtista)
+                        && (!string.IsNullOrEmpty(buscaAlbum)
+                        ? f.Album.Titulo.Contains(buscaAlbum)
+                        : true)
+                        orderby f.Album.Titulo, f.Nome
+                        select f;
+
+            foreach (var faixa in query)
+            {
+
+                Console.WriteLine("{0}\t{1}", faixa.Album.Titulo.PadRight(40), faixa.Nome);
+            }
+        }
+    }
+
+    //metodo de extensão do Linq personalizado de acordo com a necessidade
+    static class LinqExtension
+    {
+        public static decimal Mediana<TSource>(this IQueryable<TSource> source, Expression<Func<TSource, decimal>> selector)
+        {
+            var contagem = source.Count();
+
+            var funcSelector = selector.Compile();
+
+            var queryOrdenada = source.Select(funcSelector).OrderBy(total => total);
+
+            var elementCentral_1 = queryOrdenada.Skip(contagem / 2).First();
+
+            var elementCentral_2 = queryOrdenada.Skip((contagem - 1) / 2).First();
+
+            var mediana = (elementCentral_1 + elementCentral_2) / 2;
+
+            return mediana;
+        }
+
+    }
+
+    class Genero
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+    class Musica
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int GeneroId { get; set; }
+    }
 }
